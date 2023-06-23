75414a3853d492351ca70cc0965a337d6c0b0303
jiho Kim <jimmy7335@gmail.com>
2023-06-23 17:23:11 +0900

    login finish

--- vacation/vacation/urls.py
+++ vacation/vacation/urls.py
@@ -21,4 +21,6 @@ from app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', views.home, name="home"),
+    path('login/', views.login, name="login"),
+    path('signup/', views.signup, name="signup"),
 ]
