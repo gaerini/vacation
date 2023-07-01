7a2318efc2662860bcc646e8549d57d72e98c7d4
tiger-seonbi <myungkyun99@gmail.com>
2023-07-01 22:53:34 +0900

    modal finished

--- vacation/app/static/js/timetable.js
+++ vacation/app/static/js/timetable.js
@@ -15,23 +15,7 @@ const timeblock = () => {
 
   })
     .then((response) => response.json())
-    .then((data) => {
-        const postResult = document.querySelector(".planCircleBox");
-        postResult.innerHTML = "추가 완료!";
-    //   var minSector;
-    //   var maxSector;
-    //   data.forEach(function(sector) {
-    //     for (let i = 0; i < hour24.length; i++) {
-    //       if (sector.endtime >= parseInt(hour24[i].display) && sector.starttime <= parseInt(hour24[i].display)) {
-    //         ctx.beginPath();
-    //         ctx.moveTo(centerX, centerY);
-    //         ctx.arc(centerX, centerY, radius, (Math.PI / 180) * hour24[i].start, (Math.PI / 180) * hour24[i].end, false);
-    //         ctx.fillStyle = selectedColor;
-    //         ctx.fill();
-    //         ctx.closePath();
-    //       }
-    //   }
-      });
+    
       
     };
 
